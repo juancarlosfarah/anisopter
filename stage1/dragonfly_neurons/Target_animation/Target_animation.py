a54e142e3c26097096040a91a92e1ee2fea29913
Erik Grabljevec <eg1114@corona24.doc.ic.ac.uk>
2015-01-28 15:00:03 +0000

    Created first simple animation, where N flies through the screen in parallel.

--- stage1/dragonfly_neurons/Target_animation/Target_animation.py
+++ stage1/dragonfly_neurons/Target_animation/Target_animation.py
@@ -1,6 +1,44 @@
+################################################################################
+# File: Target_animation.py
+# Author: Erik Grabljevec
+# E-mail: erikgrabljevec5@gmail.com
+# Description: Tool to animate flies.
+#              ...
+################################################################################
+
 import pyglet
 
-game_window = pyglet.window.Window(800, 600)
+# Class FlyAnimation
+# ==================
+# This class contains different possible fly animation.
+# ...
+class ParallelAnimation(pyglet.window.Window):
+    def __init__(self, N):
+        super(ParallelAnimation, self).__init__()
+        self.x = 35
+        self.y = 5
+        self.N = N
+        self.image = [pyglet.resource.image("Images/fly2.jpg") for i in range(N)]
 
-if __name__ == '__main__':
-    pyglet.app.run()
+    def update_frames(self, dt):
+        self.y += 10
+
+    def on_draw(self):
+        pyglet.gl.glClearColor(1, 1, 1, 1)
+        self.clear()
+        for i in range(self.N):
+            self.image[i].blit(self.x+100*i, self.y, 0,
+                                self.image[i].width, self.image[i].height)
+
+class FlyAnimation():
+
+    def __init__(self):
+        pass
+
+    def run_parallel(self, n):
+        window = ParallelAnimation(n)
+        pyglet.clock.schedule_interval(window.update_frames, 1/10.0)
+        pyglet.app.run()
+
+test = FlyAnimation()
+test.run_parallel(2)
