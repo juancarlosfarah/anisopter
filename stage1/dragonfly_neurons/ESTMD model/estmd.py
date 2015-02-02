a12b4ecc9e117171eea669f3025a27f772b80546
Erik Grabljevec <eg1114@corona24.doc.ic.ac.uk>
2015-02-02 14:09:50 +0000

    Added another filter, working on finishing target detection.

--- stage1/dragonfly_neurons/ESTMD model/estmd.py
+++ stage1/dragonfly_neurons/ESTMD model/estmd.py
@@ -1,14 +1,25 @@
 import cv2
 import numpy as np
+from scipy import signal
 import math
+from copy import deepcopy
 
 def G(t):
     return 1.06 * math.exp(math.pow((-math.log(t / 0.505, 10)),2) / 0.0776) - 0.3356 * math.exp(math.pow((- math.log(t / 0.875)), 2) / 0.238)
+
+def RTC_exp(T_s, x):
+    return math.exp(-T_s / x)
+
+np.vectorize(RTC_exp)
 
 cap = cv2.VideoCapture("Slow Motion Dragonfly Hunting.mov")
 
 t0 = 0.001
 t = t0
+dt = 0.001
+LMC_rec_depth = 8
+
+frame_history = []
 
 while(True):
     ret, frame = cap.read()
@@ -18,45 +29,83 @@ while(True):
 
     blue,green,red = cv2.split(frame)
 
-    if t < 0.1:
-        print "green"
-        print green
-
     # Blur and downsize image
     downsize = cv2.pyrDown(green)
-    #small = cv2.resize(downsize,(100,100))
+    downsize = 1.0 * downsize / 256.0
+    frame_history.append(downsize)
+    if len(frame_history) < LMC_rec_depth:
+        continue
 
     """
-    if t < 0.1:
+	if t < 0.1:
         print "small"
         print small
     """
     # LMC filter
 
-    downsize *= G(t)
-    
-    if t < 0.1:
-        print "G(t)"
-        print downsize
+#    if 0.005 < t < 0.055:
+#        print "Before: "
+#        print downsize
+
+    b = [0.0, 0.00006, -0.00076, 0.0044, -0.016, 0.043, -0.057, 0.1789, -0.1524]
+    a = [1.0, -4.333, 8.685, -10.71, 9.0, -5.306, 2.145, -0.5418, 0.0651]
+    n = LMC_rec_depth
+
+    downsize = signal.lfilter(b, a, frame_history[-n:])[-1]
+
+#    if 0.005 < t < 0.055:
+#        print "After: "
+#        print downsize
 
     CSscale = -1.0 / 9.0
 
     CSKernel = CSscale * np.ones((3,3))
     CSKernel[1][1] = 8.0 / 9.0
 
-    pyr = cv2.filter2D(downsize, -1, CSKernel) 
+    downsize = cv2.filter2D(downsize, -1, CSKernel) 
+
+
+    # RTC filter
+      
+    down_pos = deepcopy(downsize)
+    down_neg = deepcopy(downsize)
+    down_pos[down_pos < 0] = 0
+    down_neg[down_neg > 0] = 0
+    down_neg = -down_neg
+
+    # On first step, instead of computing just save the image.
+    if t == 0:
+        v_pos_prev = deepcopy(down_pos)
+        v_neg_prev = deepcopy(down_neg)
+        down_pos_prev = deepcopy(down_pos)
+        down_neg_prev = deepcopy(down_neg)  
+        continue
+
     
-    if t < 0.1:
-        print "pyr"
-        print pyr
+    # Do everything for pos == ON
+    thau_pos = down_pos - down_pos_prev
+    thau_pos[thau_pos > 0] = 0.001
+    thau_pos[thau_pos < 0] = 0.1
+    mult = RTC_exp(dt, that_pos)
+    v_pos = -(mult-1) * down_pos + mult * v_pos_prev
+    v_pos_prev = deepcopy(v_pos)
 
-    cv2.imshow('frame', pyr)
+    # Do everything for neg == OFF
+    thau_neg = down_neg - down_neg_prev
+    thau_neg[thau_neg > 0] = 0.001
+    thau_neg[thau_neg < 0] = 0.1
+    mult = RTC_exp(dt, that_pos)
 
-    # Increase time step
-    t += 0.001
 
+    down_pos_prev = deepcopy(down_pos)
+    down_neg_prev = deepcopy(down_neg)    
+
+    ### Rinse and repeat
+    cv2.imshow('frame', v_pos)
+    t += dt
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Run
 cap.release()
 cv2.destroyAllWindows()
