6eec8137b85d95da8c907f4c4e6f2a6f65226490
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-02-05 17:04:48 +0000

    Added sample test.

---
+++ stage1/action_selection/test_sample_generator.py
@@ -0,0 +1,43 @@
+__author__ = 'juancarlosfarah'
+
+import unittest
+
+
+class SampleGeneratorTests(unittest.TestCase):
+
+    def setUp(self):
+        """
+
+        :return:
+        """
+        return
+
+    def test_avg_hz(self):
+        """
+
+        :return:
+        """
+        #   self.failUnless()
+        return
+
+    def test_avg_hz_with_noise(self):
+        """
+
+        :return:
+        """
+        #   self.failIf()
+        return
+
+    def tearDown(self):
+        """
+
+        :return:
+        """
+        return
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
