bc93d3b69a3d038a338e0dbf4955e23c55c3ff5b
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-02-10 16:33:04 +0000

    Implemented test for avg firing rate. Passes it.

--- stage1/action_selection/test_sample_generator.py
+++ stage1/action_selection/test_sample_generator.py
@@ -1,6 +1,9 @@
 __author__ = 'juancarlosfarah'
 
 import unittest
+import poisson_pattern_generator as sg
+import numpy as np
+import math
 
 
 class SampleGeneratorTests(unittest.TestCase):
@@ -13,11 +16,35 @@ class SampleGeneratorTests(unittest.TestCase):
         return
 
     def test_avg_hz(self):
+        """ Tests if the sample has the appropriate average firing rate.
+
+        :return: Void.
         """
 
-        :return:
-        """
-        #   self.failUnless()
+        # Set up.
+        dt = 0.001
+        num_neurons = 2000
+        sample_duration = 5000
+        bin_size = 10
+        target_rate = 54.0
+        target_sigma = 2.0
+        threshold = 100
+        firing_rates = []
+        spike_trains = sg.generate_spike_trains(num_neurons, sample_duration)
+
+        # Sample rate over time bins.
+        for i in range(0, spike_trains.shape[1] - bin_size, bin_size):
+            total_spikes = np.sum(np.sum(spike_trains[:, i: i + bin_size]))
+            actual_rate = total_spikes / (num_neurons * bin_size) / dt
+            firing_rates.append(actual_rate)
+
+        # Calculate mean and standard deviation.
+        mean_rate = np.mean(firing_rates)
+        actual_sigma = np.std(firing_rates)
+
+        # Ensure that the actual figures are within the threshold.
+        self.failUnless(math.fabs(target_rate - mean_rate) < threshold)
+        self.failUnless(math.fabs(target_sigma - actual_sigma) < threshold)
         return
 
     def test_avg_hz_with_noise(self):
