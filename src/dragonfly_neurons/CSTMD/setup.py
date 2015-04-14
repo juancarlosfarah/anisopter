fd02f1a64736229b0e0661320d1862547c070269
Ioannis Kasidakis <ik311@ray33.doc.ic.ac.uk>
2015-04-14 15:50:34 +0100

    changed first_attempt to put it into the website

---
+++ src/dragonfly_neurons/CSTMD/setup.py
@@ -0,0 +1,13 @@
+__author__ = 'ioanniskasidakis'
+
+from setuptools import setup
+
+setup(name='CSTMD',
+      version='0.1',
+      description='CSTMD module.',
+      url='',
+      author='Ioannis Kasidakis',
+      author_email='ioannis.kasidakis14@imperial.ac.uk',
+      license='MIT',
+      packages=['CSTMD'],
+      zip_safe=False)
