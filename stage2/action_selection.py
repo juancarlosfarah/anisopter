a2e9c5d769685bdca70044d3c7cf8d26421095a3
Christos Kaplanis <christos.kaplanis@gmail.com>
2015-04-16 11:19:29 +0100

    first attempt at action selection, dopamine injected when first neuron spikes

--- stage2/action_selection.py
+++ stage2/action_selection.py
@@ -1,26 +1,87 @@
 from brian2 import *
+N = 4
+taum = 10*ms
+taupre = 20*ms
+taupost = taupre
+tauc = 1*second # from Izhikevich paper
+tauDop = 20*ms #  Izhikevich paper 200ms
+Ee = 0*mV
+vt = -54*mV
+vr = -60*mV
+El = -74*mV
+taue = 5*ms
+F = 15*Hz
+gmax = 1
 
-N = 100 # number of action selection neurons
-M = 100 # number of fake pattern recognition neurons
+dApre = 1 # 0.01
+dApost = -dApre * taupre / taupost * 1.05
+dApost *= gmax
+dApre *= gmax
 
-tau = 10 * ms
+sim_time = 1 * second
 
-eqs = '''dv/dt = (2-v)/tau : 1'''
+eqs_neurons = '''
+dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
+dge/dt = -ge / taue : 1
+'''
 
-G = NeuronGroup(M, eqs, threshold = 'v>0.8', reset = 'v=0')
-H = NeuronGroup(N, eqs, threshold = 'v>0.8', reset = 'v=0')
+input = PoissonGroup(N, rates=F)
+neurons = NeuronGroup(N, eqs_neurons, threshold='v>vt', reset='v = vr')
+S = Synapses(input, neurons,
+             '''
+                dApre/dt = -Apre / taupre : 1 
+                dApost/dt = -Apost / taupost : 1 
+                dDop/dt = -Dop / tauDop : 1 
+                dc/dt = -c / tauc : 1
+                dw/dt = c*Dop : 1
+             ''',
+             pre='''ge += w
+                    Apre += dApre
+                    c = c + Apost
+                    w = clip(w, 0, gmax)''',
+             post='''Apost += dApost
+                     c = c + Apre
+                     w = clip(w, 0, gmax)''',
+             connect=True,
+             )
 
-G.v = 'rand()'
+S.w = 'rand() * gmax'
+S.c = 'rand()'
 
-S = Synapses(G, H, 'w : 1', 
+mon = StateMonitor(S, ('w', 'Dop', 'c'), record=[0, 1])
+s_mon = SpikeMonitor(neurons)
+r_mon = PopulationRateMonitor(input)
 
-M = SpikeMonitor(G)
-State = StateMonitor(G, 'v', record=0)
+#run(sim_time, report='text')
 
-run(100*ms)
-
-subplot(2,1,1)
-plot(M.t/ms, M.i, '.')
-subplot(2,1,2)
-plot(State.t/ms, State.v[0])
+num_spikes = 0
+for i in range(100):
+    run(10*ms, report='text')
+    if s_mon.num_spikes > num_spikes:
+        num_spikes = s_mon.num_spikes
+        if s_mon.i[-1] == 0:
+            S.Dop += 0.5
+    
+subplot(321)
+plot(S.w / gmax, '.k')
+ylabel('Weight / gmax')
+xlabel('Synapse index')
+subplot(322)
+hist(S.w / gmax, 20)
+xlabel('Weight / gmax')
+subplot(323)
+plot(mon.t/second, mon.w.T/gmax)
+xlabel('Time (s)')
+ylabel('Weight / gmax')
+subplot(324)
+plot(s_mon.t/second, s_mon.i, '.')
+xlabel('Time (s)')
+ylabel('Neuron number')
+subplot(325)
+plot(mon.t/second, mon.Dop[0])
+ylabel('Dopamine')
+subplot(326)
+plot(mon.t/second, mon.c[0])
+ylabel('c')
+tight_layout()
 show()
