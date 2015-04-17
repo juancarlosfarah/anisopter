afca1bf90b357a325c9a1a7137f470b2efb826d5
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-04-17 20:08:52 +0100

    Printing info about CWD for animations.

--- src/server/animation_dao.py
+++ src/server/animation_dao.py
@@ -92,9 +92,11 @@ class AnimationDao:
         _id = self.save(a)
 
         # Save video file.
-        print os.getcwd()
+        print "Current working directory: " + os.getcwd() + "."
         relative_path = "assets/animations/"
         out_directory = os.path.abspath(relative_path + str(_id) + ".avi")
+        print "Saving animation in: " + out_directory + "."
+
         a.run(out_directory)
 
         return _id
