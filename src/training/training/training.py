04b2c01561f862744af7ebaa231f64465ffa4f45
eg1114 <erikgrabljevec5@gmail.com>
2015-05-12 17:56:47 +0100

    "Training-moarhhh"

--- src/training/training/training.py
+++ src/training/training/training.py
@@ -33,23 +33,35 @@ class Training(object):
 
         os.makedirs(name)
 
-    def test_create_video(self):
+    def create_video(self, start, vel, id):
         """
 
+        :param start: Starting position.
+        :param vel: Velocity vector.
+        :param id: Id of video.
         :return:
         """
 
         test = Animation()
         test.add_target(2)
+        test.add_target(2, start=start, velocity=vel, size=5, v=8)
 
         local_dir = "out_directories"
-        name = "test.avi"
+        name = "test" + str(id) + ".avi"
         path = local_dir + "/" + name
         Training.make_temp_directory(local_dir)
 
-        test.run(path, 10, 10)
+        test.run(path, 10, 20)
+
+    def test_sequence_videos(self):
+
+        starts = [[200, 10], [400, 10], [400, 300]]
+        dirs = [[0, 1], [-1, 1], [-1, 0]]
+
+        for s, d, i in zip(starts, dirs, range(len(starts))):
+            self.create_video(s, d, i)
 
 
 if __name__ == '__main__':
     t = Training()
-    t.test_create_video()
+    t.test_sequence_videos()
