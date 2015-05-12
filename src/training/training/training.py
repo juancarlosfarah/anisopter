7d4ec6c32c859920aabd2755252996f9ccd25a9f
eg1114 <erikgrabljevec5@gmail.com>
2015-05-12 17:46:34 +0100

    "Training-moarhhh"

--- src/training/training/training.py
+++ src/training/training/training.py
@@ -32,3 +32,24 @@ class Training(object):
             shutil.rmtree(name)
 
         os.makedirs(name)
+
+    def test_create_video(self):
+        """
+
+        :return:
+        """
+
+        test = Animation()
+        test.add_target(2)
+
+        local_dir = "out_directories"
+        name = "test.avi"
+        path = local_dir + "/" + name
+        Training.make_temp_directory(local_dir)
+
+        test.run(path, 10, 10)
+
+
+if __name__ == '__main__':
+    t = Training()
+    t.test_create_video()
