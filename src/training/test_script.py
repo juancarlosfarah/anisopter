ef45e2da7f9e4afa2e0e5dd2963e3bf4d59f55b0
eg1114 <erikgrabljevec5@gmail.com>
2015-05-12 16:43:14 +0100

    First function and test for training module.

--- src/training/test_script.py
+++ src/training/test_script.py
@@ -4,7 +4,7 @@ import subprocess
 
 command1 = "python setup.py install --user"
 command2 = "python -m coverage run test_training.py"
-command3 = "python -m coverage report -m training/training.py"
+command3 = "python -m coverage report training/training.py"
 
 commands = [command1, command2, command3]
 
