7a4527fada7c3cf4aaebb61c0fc08f35a5faf8f3
Juan Carlos Farah <farah.juancarlos@gmail.com>
2015-05-14 22:32:27 +0100

    Temporarily doing this for Neuron 0.

--- src/server/simulation_dao.py
+++ src/server/simulation_dao.py
@@ -224,7 +224,7 @@ class SimulationDao:
         duration = simulation["duration"]
         neurons = simulation["neurons"]
         patterns = simulation["start_positions"]
-        n = neurons[2]
+        n = neurons[0]
 
         # Split start positions and spikes into 4 arrays.
         split = duration / 4
