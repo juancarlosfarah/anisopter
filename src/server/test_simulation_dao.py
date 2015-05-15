c4ae4727282b10bcf03beb845bcebd8f79c4af56
eg1114 <erikgrabljevec5@gmail.com>
2015-05-15 01:31:34 +0100

    "Automatic"

--- src/server/test_simulation_dao.py
+++ src/server/test_simulation_dao.py
@@ -35,7 +35,7 @@ class SampleDaoTests(unittest.TestCase):
         id = self.samples.generate_sample(100, 2, 2, "Random")
         sample = self.samples.get_sample(id)
         c = self.samples.db.samples
-        cursor = c.find().sort('_id', direction=-1).limit(num_samples)
+        cursor = c.find().sort('_id', direction=-1).limit(50)
         
         id2 = self.dao_simul.run_simulation(sample, cursor, 5, "Random", 1, 1, 1, [1 for i in range(5)], False)
 
