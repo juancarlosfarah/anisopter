fba7a04d5b790a732ae7faefd3b82a01868054e5
eg1114 <erikgrabljevec5@gmail.com>
2015-05-16 23:18:30 +0100

    Changing path to absolute.

--- src/training/training/training.py
+++ src/training/training/training.py
@@ -89,7 +89,7 @@ class Training(object):
 
         out_dir = "out_directories"
         name = "test1"
-        out_path = out_dir + "/" + name
+        out_path = os.path.abspath(os.path.join(out_dir, name))
 
         self.make_temp_directory(out_dir)
         self.ani.run(out_path)
