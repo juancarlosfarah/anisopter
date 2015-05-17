16ba5b9470cc003e5436e500c7208e2718138e84
eg1114 <erikgrabljevec5@gmail.com>
2015-05-17 11:15:39 +0100

    Training.py

--- src/training/training/training.py
+++ src/training/training/training.py
@@ -94,7 +94,7 @@ class Training(object):
         out_path = out_dir + "/" + name
 
         self.make_temp_directory(out_dir)
-        print os.call(['pwd'])
+        print "Path: ", os.call(['pwd'])
         print "----------------------- Start ani: ", out_path
         self.ani.run(out_path)
         print "----------------------- Start estmd"
